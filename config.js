ea2ed1f4db656e94d7fd14bbdaf85a2d1b99ead9
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-11-01 18:48:57 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-<script>
-window.APP_CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzANcSixCAEfvqcj7fOfAQn7TaHkeYcOh3msNSeuwkK2eKwdLTqgQ0U9jCJXSyrswJW/exec",
-  ADMIN_TOKEN: "CXASTrainingPortal25",
-  ADMIN_PASS:  "cooladmin",
-  BRAND:       "Training Portal",
-  TIMEZONE:    "America/New_York"
-};
-</script>
