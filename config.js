fc6ec2f7242735eb8cf4beedf5a6f7bf552455e1
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-11-01 21:10:43 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+<script>
+window.APP_CONFIG = {
+  SCRIPT_URL: "https://script.google.com/macros/s/PASTE_YOUR_EXEC_URL/exec",
+  ADMIN_TOKEN: "CXASTrainingPortal25",
+  ADMIN_PASS: "cooladmin",
+  BRAND: "CXAS Training Portal",
+  TIMEZONE: "America/New_York",
+  VERSION: "v2"
+};
+</script>
