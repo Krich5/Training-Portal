a9d8e7fc83b0a4082016cbb91d5d256d9aa8bbcb
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-11-01 21:41:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
+<!-- Training-Portal/config.js -->
 <script>
 window.APP_CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwawMz0Vgg4PSp7sZ0cWCYkEWtEL_fDunaimL5MbmjxC05QIshFdmPcedH0y6Gctu_V/exec",
-  ADMIN_TOKEN: "CXASTrainingPortal25",
-  ADMIN_PASS: "cooladmin",
-  BRAND: "CXAS Training Portal",
-  TIMEZONE: "America/New_York",
-  VERSION: "v2"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwawMz0Vgg4PSp7sZ0cWCYkEWtEL", // ends with /exec
+  ADMIN_TOKEN: "CXASTrainingPortal25",   // must match SECRET_TOKEN in Apps Script
+  ADMIN_PASS:  "cooladmin",
+  BRAND:       "CXAS Training Portal",
+  TIMEZONE:    "America/New_York"
 };
 </script>
