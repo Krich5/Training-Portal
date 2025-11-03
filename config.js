87a226bec11397e2741765e2bc72c49ab80651f9
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-11-02 21:02:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 (function () {
   const VERSION = 'v1';
-  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyBXz7dEnenXJMEYkBFNLBDkZECMd9GAzt-xMKZjuNBe8hYDotEgpe0fvNjiRz5OySU/exec';
+  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxWJG3tt9oZLU0zm4vV8tF4q-BybwHzpxW9fHBkMT3l1D98sxq6hor60QJmjWWWASuj/exec';
   const ADMIN_TOKEN = 'CXASTrainingPortal25';
   const ADMIN_PASS = 'admin';
   const BRAND = 'CXAS Training Portal';
